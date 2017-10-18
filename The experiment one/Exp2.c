911f8316f138a659462decbe8756e434960bcc6b
amaoworks <amaoworks@gmail.com>
2017-10-18 19:16:08 +0800

    The experiment one

---
+++ The experiment one/Exp2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc ,char **argv)
+{
+    int var1, var2, var3;
+    printf("Input the number :");
+    scanf("%d", &var1);
+
+
+    return 0;
+}
