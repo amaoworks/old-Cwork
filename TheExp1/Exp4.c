eefb2d2cff9c1d36445b84c0145a132e8c96b630
amaoworks <amaoworks@gmail.com>
2017-10-23 18:50:54 +0800

    17/10/23

--- TheExp1/Exp4.c
+++ TheExp1/Exp4.c
@@ -11,19 +11,14 @@ int main(int argc,char **argv){
     int *const cptr_var1 = (int *)malloc(sizeof(int));
     int *const cptr_var2 = (int *)malloc(sizeof(int));
     int const *const cptr_cvar1 = (int *)malloc(sizeof(int));
-
+    
     printf("input the var1 \n");
     scanf("%d",&var1 );
     printf("input the cvar1 \n");
     scanf("%d",&cvar1 );
 
     //question one
-    //int times ;
-    //printf("请输入自增次数\n");
-    //scanf("%d",&times);
-    //int num ;
-    //printf("请输入加数\n");
-    //scanf("%d",&num);
+    printf("The answer of test one: \n");
     for(int i = 0; i < TIMES ; i++){
 	(var1) ++;
 	*ptr_var1 += NUM;
@@ -31,13 +26,15 @@ int main(int argc,char **argv){
     printf("the var1 : %d and the *ptr_var1 : %d\n", var1 , *ptr_var1);
 
     //question two
-    ptr_cvar1 = &var1;
+    printf("The answer of test two: \n");
+    ptr_cvar1 = &cvar1;
     for(int i = 0; i < TIMES ; i++){
 	var1 += NUM;
     }
     printf("the *ptr_cvar1 :%d\n", *ptr_cvar1);
 
     //question three
+    printf("The answer of test three: \n");
     int var2 = 2;
     *cptr_var1 = var1 ;
     *cptr_var2 = var2 ;
@@ -47,11 +44,12 @@ int main(int argc,char **argv){
     }
     printf("the *cptr_var1 : %d  and the *cptr_var2 : %d \n",*cptr_var1 ,*cptr_var2 );
     
-
     //question four
-
     
-
+    free(ptr_var1);
+    free(cptr_var1);
+    free(cptr_var2);
+    free(cptr_cvar1);
 
     return 0;
 }
