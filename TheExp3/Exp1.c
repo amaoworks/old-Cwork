f87bde35bac42a5f0c899349611844f77a83e718
amaoworks <amaoworks@gmail.com>
2017-11-03 23:58:49 +0800

    17/11/3

---
+++ TheExp3/Exp1.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main(int argc , char **argv){
+	//init the arrar
+	int size , temp ;
+	printf("Please input the size :\n");
+	scanf("%d", &size );
+	int arrar[size] ;
+	int *ptr_arr = arrar ;
+	printf("Please input an arrar :\n");
+	for(int i = 0 ; i < size ; i++ ){
+			scanf("%d", ptr_arr + i);
+	}
+
+	//the algorithm
+	for(int i = 0 ; i < ( size - 1) ; i++ ){
+		for(int j = 0 ; j < ( size - 1 - i) ; j++ )
+			if ( a[j] > a[j+1]){
+				temp = a[j] ;
+				a[j] = a[j+1] ;
+				a[j+1] = temp ;
+			}
+	}
+
+	//display the result
+	printf("The result is :");
+	for(int i = 0 ; i < size ; i++ ){
+		printf("%d\t", *(ptr_arr + i) );
+	}
+
+	return 0;
+}
