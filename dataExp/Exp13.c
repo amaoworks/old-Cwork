8673d6f4da1e6b954c5dfccdc82a1b3ff2671d96
amaoworks <amaoworks@gmail.com>
2017-12-24 18:24:06 +0800

    2017/12/24

--- dataExp/Exp13.c
+++ dataExp/Exp13.c
@@ -1,6 +1,6 @@
 /*************************************************************************
     > File Name: Exp13.c
-    > Author: Lucio
+    > Author: Yiyao Mao
     > Mail: amaoworks@gmail.com 
     > Created Time: 2017年12月24日 星期日 12时58分21秒
  ************************************************************************/
@@ -17,9 +17,11 @@ typedef struct LNode{	//线性表的单链表储存
 	struct LNode *next;
 } LNode, *LinkList;
 
-LinkList CreateList(int n);		//
+LinkList CreateList(int n);		//创建并初始化单链表
 void PrintList(LinkList L);		//输出带头结点单链表的所有元素
-int GetElem(LinkList L ,int i ,ElemType *e);	//
+int GetElem(LinkList L ,int i ,ElemType *e);	//在链表中找出指定元素的对应位置
+int ListInsert(LinkList *L ,int i ,ElemType *e);
+int ListDelete(LinkList *L , int i);
 
 LinkList CreateList(int n){
 	LNode *p , *q , *head;
@@ -61,6 +63,41 @@ int GetElem(LinkList L ,int i ,ElemType *e){
 	return OK;
 }
 
+//插入算法
+int ListInsert(LinkList *L ,int i ,ElemType *e){
+	int j = 0;
+	LNode *p , *q;
+	p = (*L)->next;
+	while(p && j<i-1){
+		p = p->next;
+		j++;
+	}
+	if(p == NULL || j>i)
+		return ERROR;
+	//q = LNode *malloc(sizeof(LNode));
+	q->data = *e;
+	q->next = p->next;
+	p->next = q;
+	return OK;
+}
+
+//删除算法
+int ListDelete(LinkList *L , int i){
+	int j = 0;
+	LNode *p , *q;
+	p = (*L)->next;
+	while(p && j<i-1){
+		p = p->next;
+		j++;
+	}
+	if(p == NULL || j>i)
+		return ERROR;
+	q = p->next;
+	p->next = q->next;
+	free(q);
+	return OK;
+}
+
 int main(int argc ,char **argv){
 	int i , n;
 	ElemType e;
@@ -79,6 +116,22 @@ int main(int argc ,char **argv){
 			printf("NO%d is %d\n", i , e);
 		else
 			printf("not exists\n");
+
+		//Here's the test part
+		printf("Attention! Test Part!\n");
+		printf("4-Insert data to LinkList:\n");
+		printf("Please input the data and the location(like 1,2):\n");
+		scanf("%d,%d", &e, &i);
+		ListInsert(&L , i , &e);
+		printf("\nPrint LinkList:\n");
+		PrintList(L);
+		printf("\n5-Delete data from LinkList:\n");
+		printf("Please input the location:\n");
+		scanf("%d", &i);
+		ListDelete(&L , i);
+		printf("\nPrint LinkList:\n");
+		PrintList(L);
+		printf("\n");
 	}else
 		printf("ERROR\n");
 	return 0;
---
+++ dataExp/Exp42.c
@@ -0,0 +1,83 @@
+/*************************************************************************
+    > File Name: Exp42.c
+    > Author: Yiyao Mao
+    > Mail: amaoworks@gmail.com 
+    > Created Time: 2017年12月24日 星期日 18时03分23秒
+ ************************************************************************/
+
+#include<stdio.h>
+#include<malloc.h>
+
+#define N 20
+typedef struct edgenode{  /*图的邻接表：邻接链表结点*/
+    int adjvex;  /*顶点序号*/
+    struct edgenode *next; /*下一个结点的指针*/
+}edgenode;
+
+typedef struct vnode{ /*图的邻接表：邻接表*/
+    char data;    /*顶点信息*/
+    int ind;      /*顶点入度*/
+    struct edgenode *link;  /*指向邻接链表指针*/
+}vnode;
+
+void createGraph_list(vnode adjlist[],int *p); /*建立有向图的邻接表*/
+void topSort(vnode g[],int n); /*拓扑排序*/
+
+void createGraph_list(vnode adjlist[],int *p){ /*建立有向图的邻接表*/
+    int i,j,n,e;
+    char v;
+    edgenode *s;
+    i=0;n=0;e=0;
+    printf("输入顶点序列(以#结束)：\n");
+    while((v=getchar())!='#')
+    {
+        adjlist[i].data=v;        /*读入顶点信息*/
+        adjlist[i].link=NULL;
+        adjlist[i].ind=0;
+        i++;
+    }
+    n=i;
+    *p=n;
+    /*建立邻接链表*/
+    printf("\n请输入弧的信息(i=-1结束)：i,j:\n");
+    scanf("%d,%d",&i,&j);
+    while(i!=-1){
+        s=(struct edgenode*)malloc(sizeof(edgenode));
+        s->adjvex=j;
+        s->next=adjlist[i].link;
+        adjlist[i].link=s;
+        adjlist[j].ind++;  /*顶点j的入度加1*/
+        e++;
+        scanf("%d,%d",&i,&j);
+    }
+    printf("邻接表:");
+    for(i=0;i<n;i++){  /*输出邻接表*/
+        printf("\n%c,%d:",adjlist[i].data,adjlist[i].ind);
+        s=adjlist[i].link;
+        while(s!=NULL){
+            printf("->%d",s->adjvex);
+            s=s->next;
+        }
+    }
+}
+
+void topSort(vnode g[],int n){ /*拓扑排序*/
+
+
+
+
+
+
+
+
+
+
+}
+
+int main(int argc ,char **argv){
+    vnode adjlist[N];
+    int n,*p;
+    p=&n;
+    createGraph_list(adjlist,p);
+    return 0;
+}
